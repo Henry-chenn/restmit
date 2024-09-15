2a20c70fa7dcc7991eaf59513b8f80e0b03ccb63
henry-chenn <hchen863@uwo.ca>
2024-09-15 06:31:21 -0400

    nothing

--- app/eventcreation/page.tsx
+++ app/eventcreation/page.tsx
@@ -1,3 +1,22 @@
-export default function EventCreation() {
-  return <></>;
+// Download the helper library from https://www.twilio.com/docs/node/install
+const twilio = require("twilio"); // Or, for ESM: import twilio from "twilio";
+
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+async function createMessage() {
+  const message = await client.messages.create({
+    body: "This is a scheduled message",
+    messagingServiceSid: "MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    scheduleType: "fixed",
+    sendAt: new Date(""),
+    to: "+15558675310",
+  });
+
+  console.log(message.body);
 }
+
+createMessage();
--- app/messaging.tsx
+++ app/messaging.tsx
@@ -1,54 +1,43 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // import { useState } from "react";
+import { api } from "@/convex/_generated/api";
+import { fetchQuery } from "convex/nextjs";
 import twilio from "twilio";
 
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
 
 export default function TextMessagingComponent() {
-//   const [formData, setFormData] = useState(new FormData());
+  //   const [formData, setFormData] = useState(new FormData());
   async function createMessage(formData: FormData) {
     "use server";
     const accountSid = process.env.TWILIO_AUTH_SID;
     const authToken = process.env.TWILIO_AUTH_TOKEN;
     const client = twilio(accountSid, authToken);
+    const user = await fetchQuery(api.sync.fetchAllUsers);
+    const phoneNumbers = user.users.map((user)=> {
+      return user.phoneNumber;
+    });
+  
     try {
-      const message = await client.messages.create({
-        body: formData.get("message")! as string,
-        from: "whatsapp:+14155238886", // Your Twilio phone number
-        to: "whatsapp:+12063965545", // The recipient's phone number
+      phoneNumbers.forEach(function (number) {
+        var message = client.messages
+          .create({
+            body: formData.get("message")! as string,
+            from: "whatsapp:+14155238886",
+            to: "whatsapp:" + number,
+          })
+          .then((message) => console.log(message.status));
       });
-
-      console.log(formData.get("message"));
-      return { success: true, messageSid: message.sid };
     } catch (error) {
       console.error("Error creating message:", error);
     }
   }
-//   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     const form = new FormData(e.currentTarget);
-//     setFormData(form);
-//     await createMessage(form); // Call the createMessage function with form data
-//   };
 
-//   return (
-//     <form onSubmit={handleSubmit}>
-//       <input
-//         name="message"
-//         className="text-black"
-//         placeholder="Enter your message"
-//       />
-//       <button type="submit">SEND MESSAGE</button>
-//     </form>
-//   );
-// }
-
-return (
+  return (
     <form action={createMessage}>
       <input name="message" className="text-black"></input>
       <button type="submit">SEND MESSAGE</button>
     </form>
   );
 }
-
